7f960931bd3893a7b4568b10f7ce22901393ada5
Jane429-waithaka <janewaithaka429@gmail.com>
2026-03-10 19:19:57 +0300

    filtered to those 30 and above

--- tv_analysis_hours.R
+++ tv_analysis_hours.R
@@ -1,10 +1,14 @@
 library(tidyverse)
 library(here)
-gss_cat
+library(dplyr)
+data(gss_cat)
+names(gss_cat)
+gss_cat$age
 
 tv_hours_table<-gss_cat %>% 
   filter(age >=30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours=mean(tvhours, na.rm=TRUE))
+
 
 write.csv(tv_hours_table,here("Tv_hours by Marital_status"))
